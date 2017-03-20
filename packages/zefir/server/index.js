f3a74d106d7614c37c905e9d188e9a3e05471f1c
Kasper Mikiewicz <ideredpl@gmail.com>
2017-03-21 00:25:49 +0100

    chore(build): enable compression

--- packages/zefir/server/index.js
+++ packages/zefir/server/index.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-console, no-var, vars-on-top, global-require */
 const path = require('path')
 const express = require('express')
-// const compression = require('compression');
+const compression = require('compression')
 
 // Dev middleware
 const addDevMiddlewares = (app, webpackConfig) => {
@@ -45,7 +45,7 @@ const addProdMiddlewares = (app, options) => {
   // compression middleware compresses your server responses which makes them
   // smaller (applies also to assets). You can read more about that technique
   // and other good practices on official Express.js docs http://mxs.is/googmy
-  // app.use(compression());
+  app.use(compression())
   app.use(publicPath, express.static(outputPath))
 
   app.get('*', (req, res) => res.sendFile(path.resolve(outputPath, 'index.html')))
