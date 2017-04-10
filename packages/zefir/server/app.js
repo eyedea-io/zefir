707855d3d8b886520ace70456c2b3b8cfce3c8b4
Kasper Mikiewicz <ideredpl@gmail.com>
2017-04-10 13:04:46 +0200

    chore(router): change to hash router

--- packages/zefir/server/app.js
+++ packages/zefir/server/app.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {BrowserRouter as Router} from 'react-router-dom'
+import {HashRouter as Router} from 'react-router-dom'
 import Index from 'src'
 import {connect} from '../lib/utils'
 import Provider from './provider'
