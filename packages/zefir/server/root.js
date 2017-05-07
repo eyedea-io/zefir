88ba771851791550bdb111b9b632ab216809083c
Kasper Mikiewicz <ideredpl@gmail.com>
2017-05-08 00:59:41 +0200

    chore

--- packages/zefir/server/root.js
+++ packages/zefir/server/root.js
@@ -2,6 +2,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import {AppContainer} from 'react-hot-loader'
+
 import App from './app'
 
 const $root = document.getElementById('root')
