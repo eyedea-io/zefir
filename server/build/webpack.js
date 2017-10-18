c0664b46c24c386433e4141d978510a4278f0d3d
Kasper Mikiewicz <ideredpl@gmail.com>
2017-10-19 00:52:21 +0200

    fix: static folder copy

--- server/build/webpack.js
+++ server/build/webpack.js
@@ -115,7 +115,8 @@ export default async function createCompiler(
     }),
     new CopyWebpackPlugin([
       {
-        from: 'src/static',
+        context: join(dir, 'src/static'),
+        from: '**/*',
         to: 'static'
       }
     ]),
